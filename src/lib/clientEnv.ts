3a8c5297c1fb77e77365cb39d133960eb1cb4e55
AmirAkbarpour <conact@amirakbarpour.com>
2025-04-23 17:55:44 +0330

    🔁 migrate from zod to valibot for validation

--- src/lib/clientEnv.ts
+++ src/lib/clientEnv.ts
@@ -1,9 +1,9 @@
 import { createEnv } from '@t3-oss/env-core'
-import * as z from '@zod/mini'
+import * as v from 'valibot'
 
 const clientEnv = createEnv({
   client: {
-    VITE_TMDB_API_READ_TOKEN: z.string().check(z.minLength(1)),
+    VITE_TMDB_API_READ_TOKEN: v.pipe(v.string(), v.minLength(1)),
   },
   runtimeEnv: import.meta.env,
   clientPrefix: 'VITE_',
